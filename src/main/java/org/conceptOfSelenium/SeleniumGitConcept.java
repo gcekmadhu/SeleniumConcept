4a449031880c3f416a94eedb38b9013c6eebe4ff
Madhuri Kulkarni <gcek.madhu@gmail.com>
2023-01-25 00:34:00 +0530

    new file

---
+++ src/main/java/org/conceptOfSelenium/SeleniumGitConcept.java
@@ -0,0 +1,7 @@
+package org.conceptOfSelenium;
+
+public class SeleniumGitConcept {
+    public static void main(String[] args){
+        System.out.println("Test GIT branching concepts");
+    }
+}
