cdf2b20b30604133138556dd941cfe094d7e517a
Madhuri Kulkarni <gcek.madhu@gmail.com>
2023-01-24 22:54:38 +0530

    Added driver quit instead of close on line 20 for WebDriverBasics file

--- src/main/java/org/conceptOfSelenium/WebDriverBasics.java
+++ src/main/java/org/conceptOfSelenium/WebDriverBasics.java
@@ -17,6 +17,6 @@ public class WebDriverBasics {
             System.out.println("Wrong title");
         }
         System.out.println("Current URL:--"+driver.getCurrentUrl());
-        driver.close();
+        driver.quit();
     }
 }
