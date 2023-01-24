50122a6fb275b948fe8e09a2d85c5538c6026317
Madhuri Kulkarni <gcek.madhu@gmail.com>
2023-01-24 22:32:42 +0530

    SeleniumBasics

---
+++ src/main/java/org/conceptOfSelenium/WebDriverBasics.java
@@ -0,0 +1,22 @@
+package org.conceptOfSelenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class WebDriverBasics {
+    public static void main(String[] args){
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com");
+        String title=driver.getTitle();
+        System.out.println("Page title"+title);
+        if(title.equals("Google")){
+            System.out.println("Title is correct");
+        }
+        else {
+            System.out.println("Wrong title");
+        }
+        System.out.println("Current URL:--"+driver.getCurrentUrl());
+        driver.close();
+    }
+}
